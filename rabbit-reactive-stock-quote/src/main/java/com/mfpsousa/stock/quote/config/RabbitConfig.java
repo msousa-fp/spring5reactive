462d2b44adf21791860c7e0a41e2a846ab0bb479
Marco Sousa <marco.sousa@diconium.com>
2023-04-01 10:49:56 +0100

    Implemented example with rabbitmq

---
+++ rabbit-reactive-stock-quote/src/main/java/com/mfpsousa/stock/quote/config/RabbitConfig.java
@@ -0,0 +1,60 @@
+package com.mfpsousa.stock.quote.config;
+
+import com.rabbitmq.client.Connection;
+import org.springframework.amqp.rabbit.connection.CachingConnectionFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import reactor.core.publisher.Mono;
+import reactor.rabbitmq.*;
+
+import javax.annotation.PreDestroy;
+
+@Configuration
+public class RabbitConfig {
+    public static final String QUEUE = "quotes";
+
+    @Autowired
+    Mono<Connection> connectionMono;
+
+    @Bean
+    Mono<Connection> connectionMono(CachingConnectionFactory connectionFactory){
+        return Mono.fromCallable(() -> connectionFactory.getRabbitConnectionFactory().newConnection());
+    }
+
+    @PreDestroy
+    public void close() throws Exception {
+        connectionMono.block().close();
+    }
+
+    @Bean
+    Sender sender(Mono<Connection> mono) {
+        return RabbitFlux.createSender(new SenderOptions().connectionMono(mono));
+    }
+
+    @Bean
+    Receiver receiver(Mono<Connection> mono) {
+        return RabbitFlux.createReceiver(new ReceiverOptions().connectionMono(mono));
+    }
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
